6afc085b3a6231970e45493e77cdc07ab6ed5d2f
dfchaves <davidchaves@tyba.com.co>
2021-12-09 20:54:42 -0500

    Add initial zoom, rotarion and tilt camera - On ready define new position, traffic and hybrid map

--- app/src/main/java/com/example/mapasudea/MapsActivity.kt
+++ app/src/main/java/com/example/mapasudea/MapsActivity.kt
@@ -3,13 +3,10 @@ package com.example.mapasudea
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.mapasudea.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -38,11 +35,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
+        val kyoto = LatLng(35.00116, 135.7681)
         mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Set the map coordinates to Kyoto Japan.
+        mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+        mMap.addMarker(MarkerOptions().position(kyoto).title("Kyoto"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(kyoto))
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15F))
+        mMap.isTrafficEnabled = true
     }
 }
